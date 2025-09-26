95ef4403529658a5ae6f0b136159ee7468e3a03b
ono.rikuto <ono.rikuto@dsic.jp>
2025-09-26 14:02:32 +0900

    iconDataファイルの追加

---
+++ app/sampleData/iconData.tsx
@@ -0,0 +1,32 @@
+export const IconData = [
+  {
+    value: 1,
+    src: "/amy-burns.png",
+    alt: "女性のイラスト",
+  },
+  {
+    value: 2,
+    src: "/balazs-orban.png",
+    alt: "男性のイラスト",
+  },
+  {
+    value: 3,
+    src: "/junping-bullfighting.png",
+    alt: "バッファローのイラスト",
+  },
+  {
+    value: 4,
+    src: "/theWoman.png",
+    alt: "女性の写真",
+  },
+  {
+    value: 5,
+    src: "/theMan.png",
+    alt: "男性の写真",
+  },
+  {
+    value: 6,
+    src: "/squirrel.png",
+    alt: "リスの写真",
+  },
+];
