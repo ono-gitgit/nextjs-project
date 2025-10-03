6ab0fa5b1816f00686dea73db87bbcf38400fba8
ono.rikuto <ono.rikuto@dsic.jp>
2025-10-03 17:32:17 +0900

    ライブラリの修正

--- app/lib/utils.tsx
+++ app/lib/utils.tsx
@@ -1,9 +1,16 @@
 //日付を「YYYY-MM-DD」の形式で表示させる処理
 export function formatDate(date: Date) {
   const year = date.getFullYear();
-  const month = date.getMonth();
+  const month = date.getMonth() + 1;
   const day = date.getDate();
   return year + "-" + month + "-" + day;
+}
+
+export function formatDateToString(date: Date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return year + "年" + month + "月" + day + "日";
 }
 
 //数値を３桁区切りで表示させる処理
