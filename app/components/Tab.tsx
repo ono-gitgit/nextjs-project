3b6e4e99ef1871bf277687d6f13c4d8af10382e6
ono.rikuto <ono.rikuto@dsic.jp>
2025-10-23 13:45:11 +0900

    コンポーネントの追加・修正

--- app/components/Tab.tsx
+++ app/components/Tab.tsx
@@ -1,6 +1,12 @@
 "use client";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ReactNode } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 type TabProp = {
   heading: string;
@@ -33,22 +39,44 @@ export default function Tab({
           <hr className="border-black border-2 rounded-[5px] w-full" />
         </h1>
       </div>
-      <Tabs defaultValue={firstTabsValues} className="mt-3">
-        <TabsList className="bg-gray-200 w-[85vw] mx-auto">
-          <TabsTrigger value={firstTabsValues} onClick={firstTabsOnClick}>
-            {firstTabsTitle}
-          </TabsTrigger>
-          <TabsTrigger value={seconsdTabsValues} onClick={secondTabsOnClick}>
-            {seconsdTabsTitle}
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value={firstTabsValues} className="max-w-[95vw]">
-          {firstTabsContent}
-        </TabsContent>
-        <TabsContent value={seconsdTabsValues} className="justify-items-center">
-          {seconsdTabsContent}
-        </TabsContent>
-      </Tabs>
+      <Accordion
+        type="single"
+        collapsible
+        className="mt-2 w-[80vw] rounded-2xl bg-[#3F9877]"
+      >
+        <AccordionItem value="item-1">
+          <AccordionTrigger className="ml-3 text-[#FAFAFA]">
+            詳細を確認する
+          </AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-4 text-balance bg-[#FAFAFA]">
+            <Tabs defaultValue={firstTabsValues} className="mt-3 w-[80vw]">
+              <TabsList className="bg-gray-200 w-[78vw] mx-auto">
+                <TabsTrigger value={firstTabsValues} onClick={firstTabsOnClick}>
+                  {firstTabsTitle}
+                </TabsTrigger>
+                <TabsTrigger
+                  value={seconsdTabsValues}
+                  onClick={secondTabsOnClick}
+                >
+                  {seconsdTabsTitle}
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent
+                value={firstTabsValues}
+                className="flex justify-center"
+              >
+                {firstTabsContent}
+              </TabsContent>
+              <TabsContent
+                value={seconsdTabsValues}
+                className="justify-items-center"
+              >
+                {seconsdTabsContent}
+              </TabsContent>
+            </Tabs>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
